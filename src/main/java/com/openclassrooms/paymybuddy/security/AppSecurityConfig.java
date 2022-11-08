54c82feeb58af7603f2d9440ca5bd9f8b14b0377
AntoineMulpas <mulpas.antoine@gmail.com>
2022-11-08 19:16:07 +0100

    Adding homepage.

--- src/main/java/com/openclassrooms/paymybuddy/security/AppSecurityConfig.java
+++ src/main/java/com/openclassrooms/paymybuddy/security/AppSecurityConfig.java
@@ -3,17 +3,11 @@ package com.openclassrooms.paymybuddy.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -29,19 +23,11 @@ public class AppSecurityConfig {
                 .anyRequest().authenticated()
                 .and()
                     .formLogin()
+                    .defaultSuccessUrl("/home")
                 .and()
                 .build();
     }
 
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new  InMemoryUserDetailsManager(
-                new User(
-                        "antoine",
-                        passwordEncoder().encode("password"),
-                        Collections.emptyList()));
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
